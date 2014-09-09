c567295eb9fdbe7c17cb6f3d397b716bf489f9d6
Chris Opperwall <chriso@ifixit.com>
2014-09-08 23:07:44 -0700

    Add jQuery effects to the homepage

---
+++ public/js/frontpage.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+   setTimeout(function() {
+      $('.main').fadeIn(1000);
+   }, 500);
+
+   setTimeout(function() {
+      $('#top-nav').slideDown(1700);
+   }, 100);
+});
